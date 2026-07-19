9036b3a3dcc8a42d471a670e2881537461ae4af1
Peter Soos <soos.peter.96@gmail.com>
2026-07-19 13:34:38 +0200

    disables logging in tests

--- internal/persistence/chores/chores_suite_test.go
+++ internal/persistence/chores/chores_suite_test.go
@@ -1,8 +1,10 @@
 package chores_test
 
 import (
+	"log/slog"
 	"testing"
 
+	"github.com/bpsoos/shiftbell/internal/logging"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +13,10 @@ func TestChores(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Chores Persistence Suite")
 }
+
+var _ = BeforeSuite(func (){
+	logging.Configure(logging.Config{
+		Level: slog.LevelInfo,
+		Handler: logging.HandlerDiscard,
+	})
+})
--- internal/persistence/choretypes/choretypes_suite_test.go
+++ internal/persistence/choretypes/choretypes_suite_test.go
@@ -1,8 +1,10 @@
 package choretypes_test
 
 import (
+	"log/slog"
 	"testing"
 
+	"github.com/bpsoos/shiftbell/internal/logging"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +13,10 @@ func TestChoretypes(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Choretypes Suite")
 }
+
+var _ = BeforeSuite(func (){
+	logging.Configure(logging.Config{
+		Level: slog.LevelInfo,
+		Handler: logging.HandlerDiscard,
+	})
+})
